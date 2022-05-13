d8abd6b47dbeafaaaf55d764995063bf1f68fabf
JRuffin24 <45408495+JRuffin24@users.noreply.github.com>
2022-05-13 11:58:43 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+
+namespace Appointment_System
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            string connStr = "server=localhost;user=sqluser;database=client_schedule;port=3306;password=passw0rd!;";
+            MySqlConnection conn = new MySqlConnection(connStr);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LoginScreen());
+        }
+    }
+}
